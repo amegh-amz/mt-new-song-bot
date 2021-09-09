d91971d1a3280ccc900afd2542ca8d93f1b86295
AMZ <80452444+amegh-amz@users.noreply.github.com>
2021-09-09 21:56:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    BOT_TOKEN = "1836226590:AAH6d3_X1GOrgUtoJP6e3gJn7vE-N-sUvVM" 
+    BOT_TOKEN = "1974769678:AAHLIAu9fEgMlPeQFMOEf6fjMnmXAnP9q18" 
     API_ID = 6624445
     API_HASH = "d7bf4c140d85cf1338afb2a5a27e7cb8"
     
